5192258cc2795c2f1e7f5280c8740b3fa6217c9e
Masmics <cherlocker3113@gmail.com>
2019-05-29 09:23:29 -0700

    App.js renders as expected

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// import {
+//   Browser as Router,
+//   Switch,
+//   Route
+// } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <>
+      Hello World!
+    </>
+  );
+}
+
